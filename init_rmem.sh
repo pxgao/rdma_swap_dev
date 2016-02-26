dc314083bca1aa21fe455a967ef627f72b4c2905
root <root@ip-10-63-156-77.ec2.internal>
2016-02-26 22:15:34 +0000

    init

---
+++ init_rmem.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+make -s
+mkdir -p swap
+if [ -n "$(cat /proc/swaps | grep /mnt/swap)" ]
+then
+  swapoff /mnt/swap
+fi
+insmod rmem.ko npages=$1
+
+
+            
+mkswap /dev/rmem0;      
+swapon /dev/rmem0;
+            
+echo 0 > /proc/sys/fs/rmem/read_bytes;
+echo 0 > /proc/sys/fs/rmem/write_bytes;
+            
