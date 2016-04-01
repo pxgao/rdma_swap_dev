f5390d285ec4a95bd16ff785071decb2015e0928
petergao <petergao@berkeley.edu>
2016-04-01 13:02:18 -0700

    npage

--- init_rmem.sh
+++ init_rmem.sh
@@ -4,7 +4,7 @@ if [ -n "$(cat /proc/swaps | grep /mnt/swap)" ]
 then
   swapoff /mnt/swap
 fi
-insmod rmem_rdma.ko npages=100 servers=10.10.49.98:18515
+insmod rmem_rdma.ko npages=10000 servers=10.10.49.98:18515
 
 for s in $(ls /dev/rmem*);
 do
